c54305f4a6b8ae44cbc9991123b527482e860de4
Ramya <ramya@ecbee.net>
2026-08-03 13:03:35 +0530

    feat(email-template): update AI rules, logo styling, wording validation, and add production build output in dist

---
+++ dist/guestezee-admin/browser/chunk-TNHWFAAL.js
@@ -0,0 +1,1 @@
+import{s as o}from"./chunk-URWATHWJ.js";var a=class e{set(t,r){localStorage.setItem(t,r)}get(t){return localStorage.getItem(t)}remove(t){localStorage.removeItem(t)}static \u0275fac=function(r){return new(r||e)};static \u0275prov=o({token:e,factory:e.\u0275fac,providedIn:"root"})};export{a};
